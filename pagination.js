9c5d8b9dfb4df9a47f3e04d52da8c23913348239
rkibart <r.kibart@gmail.com>
2020-06-25 14:38:20 +0200

    wersja wysłana v1.0

--- pagination.js
+++ pagination.js
@@ -1,4 +1,20 @@
 // Stwórz funkcję paginateArray
+// która przyjmuję array do paginacji dataEntries oraz settings o kluczach { actualPageIdx=9, entriesOnPage=50 }
+// - actualPageIdx to index wybranej strony
+// - entriesOnPage to maksymalna zwracana ilość elementów z dataEntries dla wybranej strony
+
+// który zwraca entriesOnSelectedPage:
+// - entriesOnSelectedPage to array z odpowiednią ilością elementów z danej strony
+
+
+
+// poniższa funkcja zwraca "shalow copy" odpowiedniego fragmentu array dataEntries
+// lub pustą macierz gdy dataEntries jest pusta, entriesOnPage = 0 lub actualPageIdx jest za wysoki
+// w przypadku błędnych danych wejściowych zwraca undefined
+
+// jesli byłoby uzasadnione zwrócenie niezależnej macierzy entriesOnSelectedPage przy braku informacji o strukturze dataEntry
+// skorzystałbym z funkcji do głębokiego kopiowania z bibliotek lodash lub rdfc
+
 const paginateArray = (dataEntries, settings = {}) => {
     
     
@@ -24,49 +40,8 @@ const paginateArray = (dataEntries, settings = {}) => {
     return entriesOnSelectedPage = dataEntries.slice(indexStart, indexEnd)
 }
 
-// która przyjmuję array do paginacji dataEntries oraz settings o kluczach { actualPageIdx=9, entriesOnPage=50 }
-// - actualPageIdx to index wybranej strony
-// - entriesOnPage to maksymalna zwracana ilość elementów z dataEntries dla wybranej strony
-
-// który zwraca entriesOnSelectedPage:
-// - entriesOnSelectedPage to array z odpowiednią ilością elementów z danej strony
-
-const arrayOfEntries = []
+ 
 
 
 
-for(let i = 0; i < 1040; i++) {
-    
-    arrayOfEntries.push({
-        arrayIndex: i,
-        entryIndex: i+1,
-        dimensions: {
-            x: Math.floor(Math.random()*5),
-            y: Math.floor(Math.random()*9) + 1,
-            square: function() {
-                return this.x * this.y
-            }
-        }
-    })
-}
-
-const settings1 = { actualPageIdx:9, entriesOnPage:50 }
-const settings2 = { actualPageIdx:0, entriesOnPage:5 }
-const settings3 = { actualPageIdx:50, entriesOnPage:0 }
-const settings4 = { actualPageIdx:51, entriesOnPage:10 }
-const settings5 = { actualPageIdx:11, entriesOnPage:100 }
-const settings6 = { actualPageIdx:-1, entriesOnPage:-5 }
-const settings7 = { actualPageIdx:'tyiti', entriesOnPage:100 }
-
-const result1 = paginateArray(arrayOfEntries,settings1)
-const result2 = paginateArray(arrayOfEntries,settings2)
-const result3 = paginateArray(arrayOfEntries,settings3)
-const result4 = paginateArray(arrayOfEntries,settings4)
-const result5 = paginateArray(arrayOfEntries,settings5)
-const result6 = paginateArray(arrayOfEntries,settings6)
-const result7 = paginateArray(arrayOfEntries,settings7)
-const result8 = paginateArray([],settings1)
-const result9 = paginateArray(arrayOfEntries,{})
-const result10 = paginateArray(arrayOfEntries)
-const result10 = paginateArray()
 
