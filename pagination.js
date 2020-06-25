d2bac3eef7f05eac2889028530c180b5206d419b
rkibart <r.kibart@gmail.com>
2020-06-25 16:54:42 +0200

    wersja wysłana v1.1
    Throw Error zamiast return undefined.
    Dodano warunek na istnnienie właściwych kluczy w parametrze "settings"

--- pagination.js
+++ pagination.js
@@ -10,7 +10,7 @@
 
 // poniższa funkcja zwraca "shalow copy" odpowiedniego fragmentu array dataEntries
 // lub pustą macierz gdy dataEntries jest pusta, entriesOnPage = 0 lub actualPageIdx jest za wysoki
-// w przypadku błędnych danych wejściowych zwraca undefined
+// w przypadku błędnych danych wejściowych wyrzuca błąd
 
 // jesli byłoby uzasadnione zwrócenie niezależnej macierzy entriesOnSelectedPage przy braku informacji o strukturze dataEntry
 // skorzystałbym z funkcji do głębokiego kopiowania z bibliotek lodash lub rdfc
@@ -20,17 +20,21 @@ const paginateArray = (dataEntries, settings = {}) => {
     
     //na początek sprawdzimy co dostaliśmy
     const {actualPageIdx, entriesOnPage} = settings
-    
+
     if(!Array.isArray(dataEntries)) {
-        return undefined
+        throw TypeError('The first argument must be an array')
     } 
+
+    if(actualPageIdx === undefined || entriesOnPage === undefined) {
+        throw new Error ('The second argument must be an object with keys: "actualPageIdx" , "entriesOnPage"')
+    }
     
     if(!Number.isInteger(actualPageIdx) || actualPageIdx <= 0) { // przyjmuję że strony liczymy od 1
-        return undefined
+        throw new Error('The "actualPageIdx" key must be a positive integer')
     }
     
     if(!Number.isInteger(entriesOnPage) || entriesOnPage < 0) {
-        return undefined
+        throw new Error('The "entriesOnPage" key must be a non-negative integer')
     }
     let entriesOnSelectedPage =[]
     
